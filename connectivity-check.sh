c6dd2190d4aa3011310e717240fc22f4ec1475f8
Sarthakx67 <sarthakx67@gmail.com>
2025-04-02 16:59:49 +0530

    Update connectivity-check.sh

--- connectivity-check.sh
+++ connectivity-check.sh
@@ -1,1 +1,18 @@
 
+#!/bin/bash
+
+#this is a script to check connectivity of device to site	
+
+echo "enter the site to check connectivity of in format of www.site.com : "
+
+#read the site to check connectivity of
+read site
+
+ping -c 1 $site &> /dev/null
+
+if [[ $? -eq 0 ]]
+then
+	echo "connection to $site is successful"
+else	
+	echo "connection to $site is unsuccessful"
+fi
